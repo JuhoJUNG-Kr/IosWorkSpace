6b10e14f8a782378ebd933cab802e7b12141a2fb
Juho Jung <94790678+JuhoJUNG-Kr@users.noreply.github.com>
2022-01-08 15:47:54 +0100

    변수 함수

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var name: String = "Juho"
+var age:Int = 20
+
+name
+name = "정주호"
+
+age
+age = 30
+
+let one:Int = 1
+let two:Int = 2
+
+func sayHello(name: String) -> String{
+    return "nice to meet you, \(name) "
+}
+
+sayHello(name: "juho")
+
