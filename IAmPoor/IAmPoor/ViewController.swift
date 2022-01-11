83e4fa2e0cfb93857983550c86b20aedcb10daa7
Juho Jung <94790678+JuhoJUNG-Kr@users.noreply.github.com>
2022-01-11 16:42:20 +0100

    prectice

---
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by JuhoJung on 11/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
