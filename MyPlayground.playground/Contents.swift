a9ea5af666dd04805d1aa57cbccc60a9022d2660
Juho Jung <94790678+JuhoJUNG-Kr@users.noreply.github.com>
2022-01-12 18:11:37 +0100

    array

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,20 +1,52 @@
-import UIKit
+//This is a comment
 
-var name: String = "Juho"
-var age:Int = 20
+print("Hello World")
 
-name
-name = "정주호"
+/*This is a multiple line comment*/
 
-age
-age = 30
+//This is String Interpolation
+print("text\(2+3) text")
 
-let one:Int = 1
-let two:Int = 2
+print("Hello \(2+3) world")
+print("The Result of 5+3 = \(5+3)")
 
-func sayHello(name: String) -> String{
-    return "nice to meet you, \(name) "
-}
+//Values 변수 = 변하는 값
+var a = 5
+var b = 8
+print("The value of a is \(a)")
+print("The value of b is \(b)")
 
-sayHello(name: "juho")
+var numbers = [45, 73, 195, 53]
 
+var coumputedNumbers = [numbers [0] * numbers [1], numbers[1] * numbers[2], numbers[2] * numbers[3], numbers[3] * numbers[0]]
+
+print(coumputedNumbers)
+
+//Constants 상수 = 변하지 않는 값
+
+let someting = "neverChange"
+
+//Integer 정수 = 소수점 이하의 자리가 없음 'int'
+
+//Floating Point Number 부동 소수점 'float'
+
+//Double = 소수점 많이 필요할 때
+
+//Boolean = true/false 'bool'
+
+let randomNumber1 = Int.random(in: 1...3)
+let randomNumber2 = Int.random(in: 1..<3)
+let randomNumber3 = Float.random(in: 1...3)
+print(randomNumber1)
+print(randomNumber2)
+print(randomNumber3)
+
+//array.randomElement()
+//array.shuffle()
+
+    
+    let alphabet = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    
+    let password = alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()!
+
+print(password)
