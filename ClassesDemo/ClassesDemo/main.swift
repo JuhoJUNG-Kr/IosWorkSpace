97633fcecd47493edc008c8e04124d0c26a09320
Juho Jung <jooho293@gmail.com>
2022-01-20 15:56:46 +0100

    class practice

---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,14 @@
+let skeleton = Enemy()
+print(skeleton.health)
+skeleton.move()
+skeleton.attack()
+
+let skeleton2 = Enemy()
+let skeleton3 = Enemy()
+
+let dragon = Dragon()
+dragon.wingSpan = 5
+dragon.attackStrength = 15
+dragon.talk(speech: "Die!!")
+dragon.move()
+dragon.attack()
